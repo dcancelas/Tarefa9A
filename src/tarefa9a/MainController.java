6c8ad25f90799f08d177beae68c99df26e208efa
dcancelas <55793127+DanielCancelas@users.noreply.github.com>
2020-05-16 18:38:35 +0200

    Some changes

--- src/tarefa9a/MainController.java
+++ src/tarefa9a/MainController.java
@@ -2,13 +2,18 @@ package tarefa9a;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,15 +21,14 @@ import java.util.ResourceBundle;
  * @author dcancelas
  */
 public class MainController implements Initializable {
-    public HBox hbox;
-    public VBox vbox;
     public TableView<Cliente> tablaCLientes;
     public TableColumn<Cliente, String> dniCL;
     public TableColumn<Cliente, String> nomeCL;
     public TableColumn<Cliente, String> telefonoCL;
     public TableColumn<Cliente, String> direccionCl;
     public TableColumn<Cliente, Float> debedaCL;
-    ObservableList<Cliente> listaClientes = FXCollections.observableArrayList();
+    static ObservableList<Cliente> listaClientes = FXCollections.observableArrayList();
+    static Stage stageNovoCliente = new Stage();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -36,6 +40,36 @@ public class MainController implements Initializable {
         debedaCL.setCellValueFactory(new PropertyValueFactory<>("debeda"));
         debedaCL.setStyle("-fx-alignment: CENTER-RIGHT;");
 
-        listaClientes.add(new Cliente("54233422C", "Daniel", "620534053", "Av. da Xunqueira 27, Moaña", 200000));
+        //Clientes de proba
+        /*for (int i = 0; i < 30; i++) {
+            listaClientes.add(new Cliente("342234", "Nome de proba", "543534", "Dirección de proba", 43444));
+        }
+        listaClientes.add(new Cliente("1234", "Cliente a atopar", "434535435", "Direccion a atopar", 200000));
+        for (int i = 0; i < 10; i++) {
+            listaClientes.add(new Cliente("342234", "Nome de proba", "543534", "Dirección de proba", 43444));
+        }*/
+    }
+
+    public void novoCliente(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("NovoCliente.fxml"));
+        stageNovoCliente.setTitle("Tarefa9A");
+        stageNovoCliente.setScene(new Scene(root, 360, 350));
+        stageNovoCliente.setResizable(false);
+        stageNovoCliente.show();
+    }
+
+    public void buscarCliente(ActionEvent event) {
+        String dni = JOptionPane.showInputDialog(null,"Introduzca o DNI do cliente a buscar", "Tarefa9A", JOptionPane.QUESTION_MESSAGE);
+        boolean atopado = false;
+        for (Cliente listaCliente : listaClientes) {
+            if (dni.equals(listaCliente.getDNI())) {
+                tablaCLientes.getSelectionModel().select(listaCliente);
+                tablaCLientes.scrollTo(listaCliente);
+                JOptionPane.showMessageDialog(null, "Cliente atopado!", "Tarefa9A", JOptionPane.INFORMATION_MESSAGE);
+                atopado = true;
+            }
+        }
+        if (!atopado && dni != null)
+            JOptionPane.showMessageDialog(null, "ERROR: Non se atopou un cliente co DNI introducido", "Tarefa9A", JOptionPane.ERROR_MESSAGE);
     }
 }
---
+++ src/tarefa9a/NovoCliente.java
@@ -0,0 +1,20 @@
+package tarefa9a;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+/**
+ * @author dcancelas
+ */
+public class NovoCliente {
+    public TextField dniField;
+    public TextField nomeField;
+    public TextField telfField;
+    public TextField direcField;
+    public TextField debedaField;
+
+    public void novoCliente(ActionEvent event) {
+        MainController.listaClientes.add(new Cliente(dniField.getText(), nomeField.getText(), telfField.getText(), direcField.getText(), Float.parseFloat(debedaField.getText())));
+        MainController.stageNovoCliente.close();
+    }
+}
